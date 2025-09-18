5cbdb776fbb4462c1b5fb2974174b426f634dbae
Aditi Bajaj <bajajaditi1122@gmail.com>
2025-09-18 23:04:24 +0530

    Backend completed with auth, sweets, and inventory functionality + tests

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db";
+import authRoutes from "./routes/authRoutes";
+import protectedRoutes from "./routes/protectedRoutes";
+import sweetRoutes from "./routes/sweetRoutes";
+
+
+dotenv.config();
+
+export const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/protected", protectedRoutes);
+app.use("/api/sweets", sweetRoutes);
+
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Sweet Shop API is running");
+});
+
+if (process.env.NODE_ENV !== "test") {
+  const PORT = process.env.PORT || 5000;
+
+  connectDB().then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  });
+}
+
+export default app;
