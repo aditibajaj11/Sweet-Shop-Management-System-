6752fb9a84a1976e83ee7b0daac0baa9f978f87d
Aditi Bajaj <bajajaditi1122@gmail.com>
2025-09-19 21:39:37 +0530

    updated code

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"],
-  setupFilesAfterEnv: ["<rootDir>/src/tests/setup.ts"],  // ✅ connect DB before tests
-  testTimeout: 20000,
-};
-
