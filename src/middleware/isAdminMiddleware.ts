b689eb7ff87e07fad68711ea6e36f950356e4fb3
aditibajaj11 <128385432+aditibajaj11@users.noreply.github.com>
2025-09-19 19:28:00 +0530

    Delete src directory

--- src/middleware/isAdminMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { Response, NextFunction } from "express";
-import { AuthRequest } from "./authMiddleware"; 
-
-export const isAdmin = (req: AuthRequest, res: Response, next: NextFunction) => {
-  if (req.user && req.user.role === "admin") {
-    return next();
-  }
-  return res.status(403).json({ message: "Forbidden: Admin only" });
-};
