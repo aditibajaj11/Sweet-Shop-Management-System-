121b4d5dd8545762f4bdf4cbdcc8a4d04c511bea
Aditi Bajaj <bajajaditi1122@gmail.com>
2025-09-20 22:16:39 +0530

    updated cors origin url in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,9 @@ dotenv.config();
 
 export const app: Application = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/protected", protectedRoutes);
